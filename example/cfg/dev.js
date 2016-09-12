abf8e8dc09160732143ed4cb903bf5191cd164ec
Gonzalo Rubino <grubino@WSLMWEB05001.local>
2016-09-12 17:31:28 -0300

    Fixed Dev Configuration

--- example/cfg/dev.js
+++ example/cfg/dev.js
@@ -27,13 +27,26 @@ let config = Object.assign({}, baseConfig, {
 });
 
 // Add needed loaders to the defaults here
-config.module.loaders.push({
+/*config.module.loaders.push({
   test: /\.(js|jsx)$/,
   loader: 'react-hot!babel-loader',
   include: [].concat(
     config.additionalPaths,
     [ path.join(__dirname, '/../src') ]
   )
-});
+});*/
+
+config.module.loaders = [
+  {
+    test: /\.jsx?$/,
+    loader: 'babel',
+    exclude: /node_modules/,
+    query: {
+      cacheDirectory: true,
+      presets: ['react', 'es2015']
+    }
+  }
+];
+
 
 module.exports = config;
