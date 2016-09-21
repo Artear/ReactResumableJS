e4fe4bbc0562976abe417fe8d87b6a56db1b6035
alejo <alejodaraio@gmail.com>
2016-09-21 19:03:36 -0300

    changed: functions to arrow fn. added: React.propTypes. added: React.DefaultProps. updated: webpack dev config.

--- example/src/components/ExampleForm.js
+++ example/src/components/ExampleForm.js
@@ -7,66 +7,62 @@ const ContentInside = React.createClass({
   mixins: [FRC.ParentContextMixin],
 
   propTypes: {
-      children: React.PropTypes.node
+    children: React.PropTypes.node
   },
 
   render() {
 
-      let self = this;
-
-      let optionsObject = {
-        'uploaderID': 'image-upload',
-        'filetypes': ["jpg", "png"],
-        'fileAccept':'image/*',
-        'fileAddedMessage': 'Started!',
-        'completedMessage': 'Complete! : ',
-        'service': 'http://localhost:3000/upload',
-        'textLabel': 'Uploaded files',
-        'previousText': 'Drop to upload your media:',
-        'disableDragAndDrop': true,
-        'onFileSuccess': function (files) {
-          self.props.setFiles(files);
-        },
-        'onFileAdded': function (file) {
-          file.upload();
-        },
-        'headerObject': {}
-      };
-
-      return (
-          <fieldset>
-            <p>You can add other inputs, selects or stuff right here to complete a form.</p>
-            <ReactResumableJs options={optionsObject} />
-          </fieldset>
-      );
+    return (
+      <fieldset>
+        <p>You can add other inputs, selects or stuff right here to complete a form.</p>
+        <ReactResumableJs
+          uploaderID="image-upload"
+          filetypes={["jpg", "png"]}
+          fileAccept="image/*"
+          fileAddedMessage="Started!"
+          completedMessage="Complete!"
+          service="http://localhost:3000/upload"
+          textLabel="Uploaded files"
+          previousText="Drop to upload your media:"
+          disableDragAndDrop={true}
+          onFileSuccess={(files) => {
+            this.props.setFiles(files);
+          }}
+          onFileAdded={(file, resumable) => {
+            resumable.upload();
+          }}
+      />
+  </fieldset>
+  )
+    ;
   }
 });
 
 export default class ExampleForm extends React.Component {
+
   constructor(props) {
     super(props);
+
     this.state = {
       files: []
     };
-
-    this.submit = this.submit.bind(this);
-    this.setFiles = this.setFiles.bind(this);
   }
 
-  setFiles(files) {
+  setFiles = (files) => {
     this.setState({
       files: files
     });
-  }
+  };
 
-  submit(data) {
+  submit = (data) => {
     console.log('my files', this.state.files);
     console.log('Data of the Form: ', data);
-  }
+  };
 
   render() {
     return (
-      <EmptyForm insideForm={<ContentInside setFiles={this.setFiles} />} submitValue="Publish" submitAction={this.submit}/>
+      <EmptyForm insideForm={<ContentInside setFiles={this.setFiles}/>} submitValue="Publish"
+                 submitAction={this.submit}/>
     );
   }
 
